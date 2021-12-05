5228c380ea13291d977b55b8f19dbcf9f886f8bd
Gudvin <gudvin@nextbi.ru>
2021-12-05 18:04:09 +0300

    + spring boot

--- src/main/java/website/yoborisov/graduation/GraduationApplication.java
+++ src/main/java/website/yoborisov/graduation/GraduationApplication.java
@@ -2,9 +2,10 @@ package website.yoborisov.graduation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GraduationApplication {
+public class GraduationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GraduationApplication.class, args);
